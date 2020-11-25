32976297286999e2f1cafb6f6985f3a23c82f7d3
Diego Torres <dtorresc2@miumg.edu.gt>
2020-11-24 19:21:12 -0600

    Configurando index

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+// const { options } = require('./routes/usuarios/usuarios');
+
+const app = express();
+
+// Configuracion del Puerto
+app.set('port', process.env.PORT || 3000);
+
+// Configuracion JSON
+app.use(express.json({ limit: '80MB' }));
+app.use(cors());
+
+// Iniciando Servidor
+app.listen(app.get('port'), () => {
+   console.log('Server on Port ', app.get('port'));
+})
