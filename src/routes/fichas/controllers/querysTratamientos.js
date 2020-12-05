c5fe741590f8776f8178f470e3417e054505bb52
Diego Torres <dtorresc2@miumg.edu.gt>
2020-12-04 21:27:42 -0600

    Querys tratamientos 4

--- src/routes/fichas/controllers/querysTratamientos.js
+++ src/routes/fichas/controllers/querysTratamientos.js
@@ -6,12 +6,12 @@ const registrarTratamiento = ({
 }) => {
    return new Promise((resolve, reject) => {
       const query = 'INSERT INTO tratamientos ' +
-         '(plan,costo,fecha,' +
+         '(numero,plan,costo,fecha,' +
          'id_pieza,id_servicio,id_historial_odonto) ' +
-         'VALUES (?,?,?,?,?,?)';
+         'VALUES (?,?,?,?,?,?,?)';
 
       mysqlConnection.query(query, [
-         PLAN, COSTO, FECHA, ID_PIEZA, ID_SERVICIO,
+         NUMERO, PLAN, COSTO, FECHA, ID_PIEZA, ID_SERVICIO,
          ID_HISTORIAL_ODONTO
       ], (err, rows, fields) => {
          if (!err) {
@@ -32,13 +32,15 @@ const obtenerTratamientos = ({ id }) => {
    return new Promise((resolve, reject) => {
       const query = 'SELECT ' +
          "id_tratamiento AS ID_TRATAMIENTO, " +
+         "numero AS NUMERO, " +
          "plan AS PLAN, " +
          "costo AS COSTO, " +
          "DATE_FORMAT(fecha,'%d/%m/%Y') AS FECHA, " +
          'id_pieza AS ID_PIEZA, ' +
          'id_servicio AS ID_SERVICIO, ' +
          'id_historial_odonto AS ID_HISTORIAL_ODONTO ' +
-         'FROM tratamientos WHERE id_historial_odonto = ?';
+         'FROM tratamientos WHERE id_historial_odonto = ? ' +
+         'ORDER BY numero ';
 
       mysqlConnection.query(query, [id], (err, rows, fields) => {
          if (!err) {
@@ -51,7 +53,24 @@ const obtenerTratamientos = ({ id }) => {
    });
 }
 
+const eliminarTratamientos = ({ id }) => {
+   return new Promise((resolve, reject) => {
+      const query = 'DELETE FROM tratamientos ' +
+         'WHERE id_historial_odonto = ?';
+
+      mysqlConnection.query(query, [id], (err, rows, fields) => {
+         if (!err) {
+            resolve({ ID: id, MENSAJE: 'TRATAMIENTOS ELIMINADOS' });
+         }
+         else {
+            reject({ ID: -1, MENSAJE: "ERROR", ERROR: err });
+         }
+      });
+   });
+}
+
 module.exports = {
    registrarTratamiento: registrarTratamiento,
-   obtenerTratamientos : obtenerTratamientos
+   obtenerTratamientos: obtenerTratamientos,
+   eliminarTratamientos: eliminarTratamientos
 }
