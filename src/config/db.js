33abbfb1ac915f2d7f31cebb5bb04128e66d5aec
Diego Torres <dtorresc2@miumg.edu.gt>
2020-12-14 19:32:07 -0600

    Ajustes con bd y perfil

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,8 @@ const mysqlConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectTimeout : 30000
 });
 
 mysqlConnection.connect(function (err){
