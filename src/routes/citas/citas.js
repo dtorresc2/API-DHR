52e2a3b5a375f50685a60e928866537f8f0a90ff
Diego Torres <dtorresc2@miumg.edu.gt>
2021-11-25 18:53:15 -0600

    Citas - Implementacion de Rutas 2

--- src/routes/citas/citas.js
+++ src/routes/citas/citas.js
@@ -8,4 +8,24 @@ router.post('/citas/consulta', async (req, res) => {
    res.json(resultado);
 });
 
+router.post('/citas/registro', async (req, res) => {
+   const resultado = await querysCitas.registrarCita(req.body);
+   res.json(resultado);
+});
+
+router.post('/citas/actualiza', async (req, res) => {
+   const resultado = await querysCitas.actualizarCita(req.body);
+   res.json(resultado);
+});
+
+router.post('/citas/actualiza/estado', async (req, res) => {
+   const resultado = await querysCitas.actualizarEstado(req.body);
+   res.json(resultado);
+});
+
+router.post('/citas/elimina', async (req, res) => {
+   const resultado = await querysCitas.eliminarCita(req.body);
+   res.json(resultado);
+});
+
 module.exports = router;
--- src/routes/citas/controllers/querysCitas.js
+++ src/routes/citas/controllers/querysCitas.js
@@ -17,6 +17,92 @@ const obtenerListadoCitas = ({ ID_USUARIO, ID_CITA }) => {
    });
 }
 
+// Registro de Citas
+const registrarCita = ({
+   DESCRIPCION,
+   FECHA,
+   REALIZADO,
+   ID_PACIENTE,
+   ID_USUARIO
+}) => {
+   return new Promise((resolve, reject) => {
+      const query = 'CALL pa_citas_crear_editar(1,0,?,?,?,?,?)';
+
+      mysqlConnection.query(query, [DESCRIPCION, FECHA, REALIZADO, ID_PACIENTE, ID_USUARIO], (err, rows, fields) => {
+         if (!err) {
+            resolve(rows[0]);
+         }
+         else {
+            reject({ ID: -1, MENSAJE: "ERROR", ERROR: err });
+         }
+      });
+   });
+}
+
+// Actualizar Cita
+const actualizarCita = ({
+   ID_CITA,
+   DESCRIPCION,
+   FECHA,
+   REALIZADO,
+   ID_PACIENTE,
+   ID_USUARIO
+}) => {
+   return new Promise((resolve, reject) => {
+      const query = 'CALL pa_citas_crear_editar(2,?,?,?,?,?,?)';
+
+      mysqlConnection.query(query, [ID_CITA, DESCRIPCION, FECHA, REALIZADO, ID_PACIENTE, ID_USUARIO], (err, rows, fields) => {
+         if (!err) {
+            resolve(rows[0]);
+         }
+         else {
+            reject({ ID: -1, MENSAJE: "ERROR", ERROR: err });
+         }
+      });
+   });
+}
+
+const actualizarEstado = ({
+   ID_CITA,
+   ID_USUARIO,
+   REALIZADO
+}) => {
+   return new Promise((resolve, reject) => {
+      const query = 'CALL pa_citas_actualizar_realizado(?,?,?)';
+
+      mysqlConnection.query(query, [ID_CITA, ID_USUARIO, REALIZADO], (err, rows, fields) => {
+         if (!err) {
+            resolve(rows[0]);
+         }
+         else {
+            reject({ ID: -1, MENSAJE: "ERROR", ERROR: err });
+         }
+      });
+   });
+}
+
+const eliminarCita = ({
+   ID_CITA,
+   ID_USUARIO
+}) => {
+   return new Promise((resolve, reject) => {
+      const query = 'CALL pa_citas_eliminar(?,?)';
+
+      mysqlConnection.query(query, [ID_CITA, ID_USUARIO], (err, rows, fields) => {
+         if (!err) {
+            resolve(rows[0]);
+         }
+         else {
+            reject({ ID: -1, MENSAJE: "ERROR", ERROR: err });
+         }
+      });
+   });
+}
+
 module.exports = {
-   obtenerListadoCitas: obtenerListadoCitas
+   obtenerListadoCitas: obtenerListadoCitas,
+   registrarCita: registrarCita,
+   actualizarCita: actualizarCita,
+   actualizarEstado: actualizarEstado,
+   eliminarCita: eliminarCita
 }
