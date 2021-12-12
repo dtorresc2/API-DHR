76f1237a917ff480e40b9ba95f9555adef950ed8
Diego Torres <dtorresc2@miumg.edu.gt>
2021-12-12 00:39:22 -0600

    Seguridad - Implementando JWT 1

---
+++ src/config/guardia.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+// Configuracion archivo - variables de entorno
+const envFile = "./src/config/.env";
+dotenv.config({ path: envFile });
+
+const rutasProtegidas = (req, res, next) => {
+   const token = req.headers["x-access-dhr-token"];
+
+   if (!token) {
+      return res.status(403).json({ MENSAJE: 'EL TOKEN ES REQUERIDO' });
+   }
+   try {
+      const decoded = jwt.verify(token, process.env.S3_PUBLIC,);
+      req.user = decoded;
+   } catch (err) {
+      return res.status(401).json({ MENSAJE: 'TOKEN INVALIDO' });
+   }
+   return next();
+};
+
+module.exports = rutasProtegidas;
--- src/routes/servicios/servicios.js
+++ src/routes/servicios/servicios.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const querysServicios = require('./controllers/querysServicios');
+const guardia = require('./../../config/guardia');
 
 router.post('/servicios', async (req, res) => {
    const resultado = await querysServicios.registrarServicio(req.body);
    res.json(resultado);
 });
 
-router.post('/servicios/listado', async (req, res) => {
+router.post('/servicios/listado', guardia, async (req, res) => {
    const resultado = await querysServicios.obtenerListadoServicios(req.body);
    res.json(resultado);
 });
