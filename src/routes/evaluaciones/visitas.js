563f84e0e932745d0124f59c264c9595379c877c
Diego Torres <dtorresc2@miumg.edu.gt>
2022-02-22 21:53:54 -0600

    Implementacion de Visitas 1

--- src/routes/evaluaciones/visitas.js
+++ src/routes/evaluaciones/visitas.js
@@ -5,6 +5,8 @@ const funcionesS3 = require('./../../config/s3');
 const guardia = require('./../../config/guardia');
 
 const querysVisitas = require('./controllers/querysVisitas');
+const querysEvaluaciones = require('./controllers/querysEvaluaciones');
+const querysPacientes = require('./../pacientes/controllers/querysPacientes');
 
 router.post('/evaluaciones/visitas/registro', guardia, async (req, res) => {
    const FECHA_ACTUAL = moment().tz("America/Guatemala").format('YYYY/MM/DD HH:mm:ss');
@@ -18,10 +20,6 @@ router.post('/evaluaciones/visitas/registro', guardia, async (req, res) => {
          element.FECHA = FECHA_ACTUAL;
          const resultado = await querysVisitas.registrarVisita(element);
       }
-      // Actualizacion de Saldos - Ficha
-      // const resultadoSaldoFicha = await querysFichas.actualizarSaldoFicha(resultadoFichas.ID_USUARIO, resultadoFichas.ID_FICHA);
-      // Actualizavion de Saldos - Pacientes
-      // const resultadoSaldoPaciente = await querysPacientes.actualizarSaldoPaciente(resultadoFichas.ID_USUARIO, resultadoFichas.ID_PACIENTE);
       res.json({ MENSAJE: 'VISITAS REGISTRADAS', CUENTA: contador });
    }
    else {
@@ -32,14 +30,23 @@ router.post('/evaluaciones/visitas/registro', guardia, async (req, res) => {
 router.post('/evaluaciones/visitas/actualizar', guardia, async (req, res) => {
    let contador = 0;
 
-   if (req.body.VISITAS.length > 0) {
-      let arreglo = req.body.VISITAS;
-      const resultadoLimpiaVisitas = await querysVisitas.eliminarVisitas(arreglo[0].ID_EVALUACION);
+   if (req.body.VISITAS.DETALLE.length > 0) {
+      let arreglo = req.body.VISITAS.DETALLE;
+      let ID_EVALUACION = req.body.VISITAS.DETALLE[0].ID_EVALUACION;
+      let ID_USUARIO = req.body.VISITAS.ID_USUARIO;
+      let ID_PACIENTE = req.body.VISITAS.ID_PACIENTE;
+      const resultadoLimpiaVisitas = await querysVisitas.eliminarVisitas(ID_EVALUACION);
 
       for (let element of arreglo) {
          contador++;
          const resultado = await querysVisitas.registrarVisita(element);
       }
+
+      // Actualizacion de Saldos - Ficha
+      const resultadoSaldoFicha = await querysEvaluaciones.actualizarSaldoEvaluacion(ID_USUARIO, ID_EVALUACION);
+      // Actualizavion de Saldos - Pacientes
+      const resultadoSaldoPaciente = await querysPacientes.actualizarSaldoPaciente(ID_USUARIO, ID_PACIENTE);
+
       res.json({ MENSAJE: 'VISITAS REGISTRADAS', CUENTA: contador });
    }
    else {
